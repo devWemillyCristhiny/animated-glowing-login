26cb590d7576c404e372b861dc1861e4a67e9ab2
devWemillyCristhiny <devwemilly.frontend@gmail.com>
2020-11-25 18:31:09 -0300

    Terminado projeto

--- src/components/AnimatedGlowingLogin.js
+++ src/components/AnimatedGlowingLogin.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import './AnimatedGlowingLogin.css'
 
 export default function(){
     return (
-        <div>
-            teste
+        <div className="container">
+            <div className="form">
+                <h2>Login Form</h2>
+                <form>
+                    <div className="inputBox">
+                        <input type="text" name="" placeholder="Username"></input>
+                    </div>
+                    <div className="inputBox">
+                        <input type="password" name="" placeholder="password"></input>
+                    </div>
+                    <div className="inputBox">
+                        <input type="submit" value="Login"></input>
+                    </div>
+                    <p>Forget Password ? <a href="#">
+                        Click Here</a></p>
+                </form>
+            </div>
         </div>
     )
 }
